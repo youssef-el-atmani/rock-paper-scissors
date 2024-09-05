37c0c54965d5b11c6a7a1a14850b6c079aa5afeb
Youssef El Atmani <shihaabyoussef1447@gmail.com>
2024-09-05 08:52:14 +0000

    Remove unnecessary comments

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// DECLARE a var named computerChoice, that store randomly one of : ROCK, PAPER, SCISSORS,      initialize it with empty string ""
 let computerChoice = "";
-// DECLARE a var named userChoice, that will store the user choice, and                         initialize it with empty string ""
 let userChoice = "";
 
-// DECLARE a var named computerScore, and initialize it with 0
 let computerScore = 0;
-// DECLARE a var named userScore,     and initialize it with 0
 let userScore = 0;
 
-// DECLARE a var named roundCounter, and initialize it with 1
 let roundCounter = 1;
-// DECLARE a var named roundWinner, and initialize it with empty string: ""
 let roundWinner = "";
 
 // getComputerChoice() returns one of: "ROCK, PAPER, SCISSORS"
